14aff1e1790f977f5b26189fbd8a27c66740143f
robsonharlley <robsonharlley@gmail.com>
2026-01-21 14:47:50 -0300

    Criação do projeto

---
+++ primeiro_programa_java/src/primeiro_programa_java/PrimeiraClasseJava.java
@@ -0,0 +1,10 @@
+package primeiro_programa_java;
+
+public class PrimeiraClasseJava {
+	/*  main é um método auto executavel*/
+	public static void main(String[] args) {
+		/*escreve texto no console*/
+		System.out.println("Meu primeiro programa em java");
+	}
+
+}
