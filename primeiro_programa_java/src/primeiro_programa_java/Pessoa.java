dabead37541ced041d5e07d66a91d7c7f41e8434
robsonharlley <robsonharlley@gmail.com>
2026-01-21 15:50:07 -0300

    adicionando campo de idade da classe pessoa

--- primeiro_programa_java/src/primeiro_programa_java/Pessoa.java
+++ primeiro_programa_java/src/primeiro_programa_java/Pessoa.java
@@ -2,4 +2,36 @@ package primeiro_programa_java;
 
 public class Pessoa {
 
+	private String nome;
+	
+	public int getIdade() {
+		return idade;
+	}
+
+	public void setIdade(int idade) {
+		this.idade = idade;
+	}
+
+	private String sobrenome;
+	
+	private int idade;
+
+	public String getNome() {
+		return nome;
+	}
+
+	public void setNome(String nome) {
+		this.nome = nome;
+	}
+
+	public String getSobrenome() {
+		return sobrenome;
+	}
+
+	public void setSobrenome(String sobrenome) {
+		this.sobrenome = sobrenome;
+	}
+	
+	
+	
 }
